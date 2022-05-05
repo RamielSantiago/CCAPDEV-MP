1436b9ac350289389d32f20f09f5df8618f0bbe3
ramonmapua <104897003+ramonmapua@users.noreply.github.com>
2022-05-05 16:24:46 +0800

    Delete popup.js
    Functions in this were added to posts.js

--- Phase 1/Javascript/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("popup").addEventListener("click", function(){
-    document.querySelector(".writePosts").classList.add("active");
-    document.querySelector("#popup").style.display="none";
-});
-document.getElementById("closeBtn").addEventListener("click", function(){
-    document.querySelector(".writePosts").classList.remove("active");
-    document.querySelector("#popup").style.display="block";
-});
