056c18476f1717c52a6915680a0332f8d39f0cda
RamielSantiago <80898792+RamielSantiago@users.noreply.github.com>
2022-07-06 12:48:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.set("views", "./views");
 //Database Connection
 db.connect();
 
-/*404 not found page
+404 not found page
 app.use((req, res) => {
     res.status(404).render("404", {
         title: "404 not found",
         customCSS1: '<link rel="stylesheet" type="text/css" href="/css/404.css">',
     });
-});*/
+});
 
 
 //------- Session Settings -------//
